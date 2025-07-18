dd59a0b1d05fe2aa543df395ad3ccb07ded77813
RF2034 <namachocoeclairsand@gmail.com>
2025-07-19 03:23:48 +0900

    参加方法の説明文を修正し、より明確にしました

--- src/components/about/AboutParticipationCard.tsx
+++ src/components/about/AboutParticipationCard.tsx
@@ -4,9 +4,10 @@ export default function AboutParticipationCard() {
       <div className="card-body">
         <h2 className="card-title">参加方法</h2>
         <ol className="list-decimal list-inside">
-          <li>トップページの「参加登録はこちら」ボタンをクリックします。</li>
-          <li>Googleフォームに必要事項を入力し、送信してください。</li>
-          <li>参加登録後、詳細な情報をDMまたはメールでお送りします。</li>
+          <li>参加登録は、公式サイトから行えます。</li>
+          <li>トップページの「参加登録はこちら」ボタンをクリックしてください。</li>
+          <li>その後、googleフォームに必要事項を入力し、送信してください。</li>
+          <li>参加登録後、詳細な情報をメールでお送りします。</li>
         </ol>
       </div>
     </div>
